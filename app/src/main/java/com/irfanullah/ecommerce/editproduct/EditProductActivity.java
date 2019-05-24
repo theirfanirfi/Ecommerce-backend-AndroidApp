d36fc7153e207247031e9e5949ac51a12b900365
Irfan Ullah <theirfanullah@gmail.com>
2019-05-24 15:02:15 +0500

    layout and errors fixed

--- app/src/main/java/com/irfanullah/ecommerce/editproduct/EditProductActivity.java
+++ app/src/main/java/com/irfanullah/ecommerce/editproduct/EditProductActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -131,5 +132,20 @@ public class EditProductActivity extends AppCompatActivity implements EditProduc
 
     private void makeImageViewVisible(){
         product_image_view.setVisibility(View.VISIBLE);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
